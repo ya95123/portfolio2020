fbf0b0e7a991b7963a0676c3a38b130df9246b41
ya95123 <ya95123@gmail.com>
2020-09-07 16:27:46 +0800

    img

--- portfolio/src/views/Home.vue
+++ portfolio/src/views/Home.vue
@@ -1,16 +1,10 @@
 <template>
-  <div id="home">
-    <h1 style="font-size:100px">home</h1>
-    <h1 style="font-size:100px">home</h1>
-    <h1 style="font-size:100px">home</h1>
-    <h1 style="font-size:100px">home</h1>
-    <h1 style="font-size:100px">home</h1>
-    <h1 style="font-size:100px">home</h1>
-    <h1 style="font-size:100px">home</h1>
-    <h1 style="font-size:100px">home</h1>
-    <h1 style="font-size:100px">home</h1>
-    <h1 style="font-size:100px">home</h1>
-  </div>
+  <v-container id="home">
+    <v-row class="justify-center">
+      <v-col cols="12" class="text-center">功能型網站</v-col>
+      <v-col cols="6"><v-img src=""></v-img></v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
