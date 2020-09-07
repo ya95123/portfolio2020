5fa6752ad2e4391dddd68154a3d31db699766032
ya95123 <ya95123@gmail.com>
2020-09-07 20:09:29 +0800

    web：structure

--- portfolio/src/views/Home.vue
+++ portfolio/src/views/Home.vue
@@ -1,10 +1,46 @@
 <template>
-  <v-container id="home">
-    <v-row class="justify-center">
-      <v-col cols="12" class="text-center">功能型網站</v-col>
-      <v-col cols="6"><v-img src=""></v-img></v-col>
-    </v-row>
-  </v-container>
+  <div id="home">
+    <!-- type1：功能型網站 -->
+    <v-container>
+      <v-row class="justify-center">
+        <v-col cols="12" class="text-center">功能型網站</v-col>
+        <v-col cols="12" md="6">
+          <v-img src="../../public/img/web/d_hand_guesse.png" class="rounded"></v-img>
+        </v-col>
+        <v-col cols="12" md="6">
+          describe
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <!-- type2：網頁切版 -->
+    <v-container>
+      <v-row class="justify-center">
+        <v-col cols="12" class="text-center">網頁切版</v-col>
+        <v-col cols="12" md="4">
+          <v-card>
+            <v-img src="../../public/img/web/onepage.png"></v-img>
+            <v-card-title>123</v-card-title>
+            <v-card-text>123</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <!-- type2：網頁小遊戲 -->
+    <v-container>
+      <v-row class="justify-center">
+        <v-col cols="12" class="text-center">網頁小遊戲</v-col>
+        <v-col cols="12" md="4">
+          <v-card>
+            <v-img src="../../public/img/web/onepage.png"></v-img>
+            <v-card-title>123</v-card-title>
+            <v-card-text>123</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
